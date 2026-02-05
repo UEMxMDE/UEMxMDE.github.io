83b9c7b4f7ed07e0d4e7f297bca2bf1e6eff8d50
UEMxMDE <70921784+UEMxMDE@users.noreply.github.com>
2026-02-04 21:22:56 -0800

    Small changes to universal.js

--- assets/javascript/universal.js
+++ assets/javascript/universal.js
@@ -1,6 +1,5 @@
-// Update this manually before committing changes to github
-const SITE_LAST_UPDATED = "February 4, 2026";
-const SITE_NAME = "wiki_name";
+// Footer date information
+const SITE_LAST_UPDATED = "February 4, 2026";	// Update this manually before committing changes to github
 document.addEventListener("DOMContentLoaded", () => {
 	const el = document.getElementById("site-last-updated");
 	if (!el) return;
