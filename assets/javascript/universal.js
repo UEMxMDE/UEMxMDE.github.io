537c8f1ba18edc055c83ff7d55b31942a50b7695
UEMxMDE <70921784+UEMxMDE@users.noreply.github.com>
2026-03-02 17:13:36 -0800

    Updated the "Website last updated on ..." message to be automated

--- assets/javascript/universal.js
+++
@@ -1,8 +0,0 @@
-// Footer date information
-const SITE_LAST_UPDATED = "February 8, 2026";	// Update this manually before committing changes to github
-document.addEventListener("DOMContentLoaded", () => {
-	const el = document.getElementById("site-last-updated");
-	if (!el) return;
-	
-	el.textContent = SITE_LAST_UPDATED;
-});
